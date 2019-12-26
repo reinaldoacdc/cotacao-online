f80f8de000e3e951248cd97f80e606637d516ebc
Reinaldo ACDC <reinaldoacdc@gmail.com>
2019-12-26 14:15:16 -0300

    Add views to Supplier

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_203558) do
+ActiveRecord::Schema.define(version: 2019_12_26_165612) do
 
   create_table "orders", force: :cascade do |t|
     t.string "company_name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_12_10_203558) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "cnpj"
     t.index ["email"], name: "index_suppliers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_suppliers_on_reset_password_token", unique: true
   end
