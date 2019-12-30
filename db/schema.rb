28fa6ef6dcabd9b8edb5a6c0ef3127aa5372c948
Reinaldo ACDC <reinaldoacdc@gmail.com>
2019-12-30 12:09:31 -0300

    Add roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_27_184816) do
+ActiveRecord::Schema.define(version: 2019_12_30_142855) do
 
   create_table "orders", force: :cascade do |t|
     t.string "company_name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_12_27_184816) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "supplier_id"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["supplier_id"], name: "index_users_on_supplier_id"
