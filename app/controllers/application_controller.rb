468f0c40b923ecafc71d1fa739ccac8cee01bee9
Reinaldo ACDC <reinaldoacdc@gmail.com>
2019-12-30 11:21:49 -0300

    Add devise multiple user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
     protected
 
          def configure_permitted_parameters
-              devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :cnpj, :email, :password)}
+              devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :cnpj, :email, :password, :password_confirmation, :supplier_id)}
 
               devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :cnpj, :email, :password, :current_password)}
          end
