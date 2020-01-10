69baf19f7e8d542b75f38cd060bc8546142a6008
Reinaldo ACDC <reinaldoacdc@gmail.com>
2020-01-10 17:39:53 -0300

    add mail production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,14 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: "gmail.com",
+    port: 587,
+    user_name: ENV["MAIL_USER"],
+    password: ENV["MAIL_PASSWORD"],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }  
 end
